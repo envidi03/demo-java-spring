148f4cc7ff827036d38fee0aa0c536f8a20d819c
Dinh <Dinhnvhe172516@fpt.edu.vn>
2024-11-18 15:33:31 +0700

    beans

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
+		ConfigurableApplicationContext demo = SpringApplication.run(DemoApplication.class, args);
+		for (String string : demo.getBeanDefinitionNames()) {
+			System.out.println(string);
+		}
 	}
 
 }
